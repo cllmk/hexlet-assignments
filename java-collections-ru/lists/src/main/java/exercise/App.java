db38f9637b86a383d7ac279d05390e9a892ce243
@hexlet/cli <support@hexlet.io>
2023-09-23 22:10:26 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,17 +6,18 @@ import java.util.List;
 
 // BEGIN
 class App {
-    public static boolean scrabble(String characterSet, String word){
-        ArrayList<String> charSet = new ArrayList<String>(Arrays.asList(characterSet.split("")));
-        ArrayList<String> charSetWord = new ArrayList<String>(Arrays.asList(word.split("")));
+    public static boolean scrabble(String characterSet, String word) {
+        ArrayList<String> charSet = new ArrayList<String>(Arrays.asList(characterSet.toLowerCase().split("")));
+        ArrayList<String> charSetWord = new ArrayList<String>(Arrays.asList(word.toLowerCase().split("")));
         int tempSize = charSet.size();
         for (int i = 0; i < charSetWord.size(); i++) {
             charSet.remove(charSetWord.get(i));
         }
-        if (charSet.size() + charSetWord.size() == tempSize){
+        if (charSet.size() + charSetWord.size() == tempSize) {
             return true;
         } else {
             return false;
         }
     }
+}
 //END
